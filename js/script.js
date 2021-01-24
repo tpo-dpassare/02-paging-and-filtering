e187b77cedf8a06c638db50e4dff8ce0c381961d
David Passarelli <dpassare@travelers.com>
2021-01-23 19:35:33 -0500

    First test passing.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 'use strict'
+
+const uiContainers = {
+  students: document.querySelector('ul.student-list')
+}
+
+for (let i = 0; i < 9; i++) {
+  uiContainers.students.insertAdjacentHTML('beforeend', `<li>${i}</li>`)
+}
